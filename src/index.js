a1a79612da3f89cc588d52375e3da450985efbb2
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-03-15 13:32:34 +0000

    Create the function that hits the Visual Crossing API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=QXFQ32SYB6Y32T7ABNSDCUC3V`,
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWeatherData("tokyo");
